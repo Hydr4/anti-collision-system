8635e1c7356b869265d133158022359c027eaf1a
Abdelrahman Moez <abdelrahman.moez@gmail.com>
2016-04-11 01:11:53 +0200

    Create ultrasonic_lcd.ino

---
+++ ultrasonic_lcd.ino
@@ -0,0 +1,27 @@
+#include <LiquidCrystal.h>
+#include <NewPing.h>
+
+#define TRIGGER 8
+#define ECHO 9
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+NewPing sonar(TRIGGER, ECHO, 50);
+
+int dist_cm;
+
+void setup() {
+  //Serial.begin(9600);
+  lcd.begin(16, 2);
+}
+
+void loop() {
+  lcd.noDisplay();
+  dist_cm = sonar.ping_cm();  
+  
+  lcd.clear();
+  lcd.print(dist_cm);
+  lcd.print(" cm");
+  lcd.display();
+  
+  delay(1000);
+}
